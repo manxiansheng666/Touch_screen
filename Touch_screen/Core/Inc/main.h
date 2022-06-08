c072bf93857640e2edd349823e1e0fcc685fa7ab
manxiansheng666 <1131620594@qq.com>
2022-06-08 17:13:21 +0800

    cubemx配置串口2和流控制引脚

--- Touch_screen/Core/Inc/main.h
+++ Touch_screen/Core/Inc/main.h
@@ -58,6 +58,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define uart2_en_Pin GPIO_PIN_7
+#define uart2_en_GPIO_Port GPIOD
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
